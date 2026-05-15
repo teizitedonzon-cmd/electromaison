78ea9625ee9338fd526eb8590e47feff84901ae5
Teizi Tedonzon <teizitedonzon@gmail.com>
2026-05-15 14:08:54 +0100

    Update Accueil.jsx

--- frontend/src/pages/client/Accueil.jsx
+++ frontend/src/pages/client/Accueil.jsx
@@ -1156,7 +1156,7 @@ export default function Accueil() {
                 </>
               ) : (
                 <>
-                  Bienvenue chez <span style={styles.brandName}>TEYSHOP</span>
+                  Bienvenue chef <span style={styles.brandName}>TEYSHOP</span>
                 </>
               )}
             </h1>
