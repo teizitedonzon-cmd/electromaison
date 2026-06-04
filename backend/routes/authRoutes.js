713c2a28265a3ec0822b7676cc333424d232c3e3
Teizi Tedonzon <teizitedonzon@gmail.com>
2026-06-04 10:40:49 +0100

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,15 @@
 
 const express = require('express');
 const router = express.Router();
-const { inscription, connexion, refresh } = require('../controllers/authController');
+const { inscription, connexion, refresh, motDePasseOublie, reinitialiserMotDePasse } = require('../controllers/authController');
 const { proteger } = require('../middleware/authMiddleware');
 const upload = require('../middleware/uploadMiddleware'); 
 
 
 router.post('/inscription', upload.single('photoProfil'), inscription);
 router.post('/connexion', connexion);
+router.post('/mot-de-passe-oublie', motDePasseOublie);
+router.post('/reinitialiser-mot-de-passe', reinitialiserMotDePasse);
 router.get('/refresh', proteger, refresh);
 
 module.exports = router;
